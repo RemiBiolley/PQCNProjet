a6ea639c9cd861adc5fca5ab1d1c7b2e150f96fc
Nigirith <fanny.streiff@gmail.com>
2018-11-20 22:45:43 +0100

    suite soluce

--- .idea/HTML/JS/Résumé.js
+++ .idea/HTML/JS/Résumé.js
@@ -1,7 +1,7 @@
 function OnOff_1() {
-    console.log(document.getElementById("sous-partie1").style.display);
-    if (document.getElementById("sous-partie1").style.display == "none" || document.getElementById("question_1").style.display == "")
-        document.getElementById("sous-partie1").style.display = "block";
+    console.log(document.getElementById("tips1").style.display);
+    if (document.getElementById("tips1").style.display == "none" || document.getElementById("tips1").style.display == "")
+        document.getElementById("tips1").style.display = "block";
     else
-        document.getElementById("sous-partie1").style.display = "none";
+        document.getElementById("tips1").style.display = "none";
 }
